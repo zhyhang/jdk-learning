5b505947e09972d5f388dc964d276718ab0cf49a
zhyhang <zhyhang1978@gmail.com>
2019-01-02 11:05:02 +0800

    string reverse simple algorithm

---
+++ jdk11-feature/src/main/java/org/yanhuang/jdk11/algorithm/StringReverse.java
@@ -0,0 +1,23 @@
+package org.yanhuang.jdk11.algorithm;
+
+public class StringReverse {
+
+	public static String reverse(String s) {
+		if (null == s) {
+			return s;
+		}
+		final int[] codePoints = s.codePoints().toArray();
+		for(int i=0,j=codePoints.length-1;i<codePoints.length/2;i++,j--){
+			int top = codePoints[j];
+			codePoints[j] = codePoints[i];
+			codePoints[i] = top;
+		}
+		return new String(codePoints,0,codePoints.length);
+	}
+
+	public static void main(String[] args) {
+		String s = "世界 really is very strong！\uD83D\uDCAA☀";
+		System.out.println(reverse(s));
+		System.out.println(new StringBuilder(s).reverse());
+	}
+}
